bc90f84484315a0fd547cd48b3cec052aff569e8
Виктор Парыгин <paryginva@live.com>
2022-01-09 01:58:28 +0500

    Добавьте файлы проекта.

---
+++ WpfApp_Task8/Task7_Command.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WpfApp_Task8
+{
+    class Task7_Command
+    {
+        public static RoutedUICommand Exit { get; set; }
+        static Task7_Command()
+        {
+            InputGestureCollection inputs = new InputGestureCollection();
+            inputs.Add(new KeyGesture(Key.T, ModifierKeys.Control, "Ctrl+T"));
+            Exit = new RoutedUICommand("Выход", "Exit", typeof(Task7_Command), inputs);
+
+        }
+    }
+}
